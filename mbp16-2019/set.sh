2a8f8f25e09d81bda1e34d11886a184b003ee06b
Denys Vitali <denys.vitali@swisscom.com>
2021-07-01 13:44:22 +0200

    initial commit

---
+++ mbp16-2019/set.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 normal|max"
+fi
+
+MODE="$1"
+SMC="/Applications/smcFanControl.app/Contents/Resources/smc"
+
+
+function to_smc_hex(){
+  VAL="$1"
+
+  python3 -c "print('%08x' % (($VAL << 2) + 1))"
+}
+
+function fan_speed(){
+  TARGET_FAN="$1"
+  SPEED="$2"
+
+  "$SMC" -k "F${TARGET_FAN}Tg" -w $(to_smc_hex "$2")
+}
+
+"$SMC" -k "F0Md" -w 01
+"$SMC" -k "F1Md" -w 01
+
+case "$MODE" in
+  auto)
+    echo "Auto speed"
+    "$SMC" -k "F0Md" -w 00
+    "$SMC" -k "F1Md" -w 00
+    ;;
+
+  min)
+    echo "MIN speed"
+    fan_speed 0 2111825
+    fan_speed 1 2111825
+    ;;
+  normal)
+    echo "NORMAL speed"
+    fan_speed 0 2238801
+    fan_speed 1 2238801
+    ;;
+
+  max)
+    echo "MAX speed"
+    fan_speed 0 1587801
+    fan_speed 1 1587801
+    ;;
+
+  *)
+    echo "Invalid speed"
+    ;;
+esac
